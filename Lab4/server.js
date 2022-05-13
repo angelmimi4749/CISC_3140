98bcb58684046b5d4aea4c060508806915317e88
Tahamina Chowdhury <tahaminachowdhury0@gmail.com>
2022-05-12 23:19:13 -0400

    Uploaded

---
+++ Lab4/server.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const app = express()
+
+const PORT = 8000
+const bodyParser = require("body-parser");
+const carRoutes = require('.routes.js')
+const cors = require('cors')
+
+app.use(cors())
+app.use(bodyParser.json())
+app.set('json spaces', 3)
+app.get("/", (req, res, next) => {
+    res.json({"message": "Ok"})
+});
+app.use("/cars", carRoutes)
+app.use(function (req, res) {
+    res.status(404);
+});
+app.listen(PORT, () => {
+    console.log("Server running on port %PORT%".replace("%PORT%", PORT))
+});
